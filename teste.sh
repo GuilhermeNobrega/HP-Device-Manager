c69edb201a6030ee89f50069ade32bb85e917d4a
Wilhelm <100214869+GuilhermeNobrega@users.noreply.github.com>
2024-04-07 22:11:58 -0300

    Create teste.sh

---
+++ teste.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for c in {1..5}
+do
+    touch "hp-341_$c"
+done
+
+#Se você deseja testar, antes de executar o Matador.sh, teste esse, pois esse vai criar vários arquivos e você pode apagar ele depois
